c513a3a62f06e9e7fd7401dcf4dc5d4ea0a72ccb
Miguel Fillipe <87400829+miguelfill@users.noreply.github.com>
2022-12-15 20:26:49 -0300

    Add files via upload

--- dino.py
+++ dino.py
@@ -2,10 +2,11 @@ import pygame
 from pygame.locals import *
 from sys import exit
 import os
+from random import randrange
 
-diretorioPrincipal = os.path.dirname(__file__)
-diretorioImagens = os.path.join(diretorioPrincipal, 'imagens')
-diretorioSons = os.path.join(diretorioPrincipal, 'sons')
+diretorio_principal = os.path.dirname(__file__)
+diretorio_imagens = os.path.join(diretorio_principal, 'imagens')
+diretorio_sons = os.path.join(diretorio_principal, 'sons')
 
 LARGURA = 640
 ALTURA = 480
@@ -16,33 +17,52 @@ tela = pygame.display.set_mode((LARGURA, ALTURA,))
 
 pygame.display.set_caption('Dino Game')
 
-spriteSheet = pygame.image.load(os.path.join(diretorioImagens, 'dinoSpritesheet.png')).convert_alpha()
+sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'dinoSpritesheet.png')).convert_alpha()
 
 
 class Dino(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.imagensDinossauro = []
+        self.imagens_dinossauro = []
         for i in range(3):
-            img = spriteSheet.subsurface((i * 32, 0), (32, 32))
-            img = pygame.transform.scale(img, (32*3, 32*3))
-            self.imagensDinossauro.append(img)
+            img = sprite_sheet.subsurface((i * 32, 0), (32, 32))
+            img = pygame.transform.scale(img, (32 * 3, 32 * 3))
+            self.imagens_dinossauro.append(img)
 
-        self.indexLista = 0
-        self.image = self.imagensDinossauro[self.indexLista]
+        self.index_lista = 0
+        self.image = self.imagens_dinossauro[self.index_lista]
         self.rect = self.image.get_rect()
-        self.rect_center = (100,100)
+        self.rect.center = (100, ALTURA - 90)
 
     def update(self):
-        if self.indexLista > 2:
-            self.indexLista = 0
-        self.indexLista += 0.25
-        self.image = self.imagensDinossauro[int(self.indexLista)]
+        if self.index_lista > 2:
+            self.index_lista = 0
+        self.index_lista += 0.25
+        self.image = self.imagens_dinossauro[int(self.index_lista)]
+
+class Nuvens(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((7 * 32, 0), (32, 32))
+        self.image = pygame.transform.scale(self.image, (32 * 3, 32 * 3))
+        self.rect = self.image.get_rect()
+        self.rect.y = randrange(50, 200, 50)
+        self.rect.x = LARGURA - randrange(30, 300, 90)
+
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = LARGURA
+            self.rect.y = randrange(50, 200, 50)
+        self.rect.x -= 10
 
 
 todasAs_Sprites = pygame.sprite.Group()
 dino = Dino()
 todasAs_Sprites.add(dino)
+
+for i in range(4):
+    nuvem = Nuvens()
+    todasAs_Sprites.add(nuvem)
 
 relogio = pygame.time.Clock()
 while True:
